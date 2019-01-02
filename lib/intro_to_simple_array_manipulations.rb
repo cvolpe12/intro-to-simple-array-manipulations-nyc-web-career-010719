4e6f4db24ce27195c1bb2573aac2c55dab4ee44b
Christopher Volpe <christopher.volpe12@gmail.com>
2019-01-02 21:20:16 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,13 @@
+def using_push(countries_in_western_africa)
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  countries_in_western_africa.push("Niger")
+end
+
+def using_unshift(neighborhoods_in_northwest_brooklyn)
+  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
+end
+
+def method_name
+
+end
