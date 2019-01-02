3e48435e2f4f82a5335d1e5424e3d6dc37c5a665
Christopher Volpe <christopher.volpe12@gmail.com>
2019-01-02 21:22:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -8,6 +8,12 @@ def using_unshift(neighborhoods_in_northwest_brooklyn)
   neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
 end
 
-def method_name
+def using_pop(great_hits_of_the_nineties)
+  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  great_hits_of_the_nineties.pop
+end
 
+def pop_with_args
+  chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
+  chars_in_game_of_thrones.pop(2)
 end
