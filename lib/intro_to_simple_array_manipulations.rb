a085a55fd96682f69673e14631545582e0361912
Christopher Volpe <christopher.volpe12@gmail.com>
2019-01-02 21:40:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,59 +1,36 @@
-def using_push(countries_in_western_africa, element)
-  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-  countries_in_western_africa.push("Niger")
+def using_push(array, element)
+  array.push(element)
 end
 
-def using_unshift(neighborhoods_in_northwest_brooklyn, element)
-  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
+def using_unshift(array, element)
 end
 
-def using_pop(great_hits_of_the_nineties)
-  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  great_hits_of_the_nineties.pop
+def using_pop(array)
 end
 
-def pop_with_args(chars_in_game_of_thrones)
-  chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane"]
-  chars_in_game_of_thrones.pop(2)
+def pop_with_args(array)
 end
 
-def using_shift(my_favorite_cities)
-  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-  my_favorite_cities.shift
+def using_shift(array)
 end
 
-def shift_with_args(ice_cream_brands)
-  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  ice_cream_brands.shift(2)
+def shift_with_args(array)
 end
 
-def using_concat(my_favorite_things)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  my_favorite_things.concat(["mario kart", "flatiron school"])
+def using_concat(array1, array2)
 end
 
-def using_insert(list_of_esoteric_programming_languages)
-  list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
-  list_of_esoteric_programming_languages.insert(4, "Malbolge")
+def using_insert(array, element)
 end
 
-def using_uniq(captain_planet_and_the_planeteers)
-  captain_planet_and_the_planeteers = ["Captain Planet", "Gaia", "Kwame", "Gi", "Linka", "Wheeler", "Gaia"]
-  captain_planet_and_the_planeteers.uniq
+def using_uniq(array)
 end
 
-def using_flatten(private_colleges_in_newyork)
-  private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
-  private_colleges_in_newyork.flatten
+def using_flatten(array)
 end
 
-def using_delete(instructors)
-  instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  instructors.delete("Steven")
+def using_delete(array, string)
 end
 
-def using_delete_at(famous_robots)
-  famous_robots = ["the dog from doctor who", "R2D2", "Ultron"]
-  famous_robots.delete(2)
+def using_delete_at(array, integer)
 end
